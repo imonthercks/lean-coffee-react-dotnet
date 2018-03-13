e1fd4838ea24afa27b99fd72d66131fe71411708
CJ Russell <imonthercks@gmail.com>
2018-03-12 23:59:35 -0500

    Added Redux and Flow dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const target = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={store}>
+      <div>
+        <App />
+      </div>
+  </Provider>,
+  target
+);
+
 registerServiceWorker();
