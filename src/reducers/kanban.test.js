ac9ba89fe17c2c99bd344c34ce035ebf0066f3f1
CJ Russell <imonthercks@gmail.com>
2018-03-18 12:09:33 -0500

    Added additional MOVE_TOPIC test and reformatted GWT

--- src/reducers/kanban.test.js
+++ src/reducers/kanban.test.js
@@ -1,9 +1,11 @@
 import { moveTopic } from '../actions/kanban'
 import reduce, { MOVE_TOPIC } from './kanban'
 
+
+
 test('MOVE_TOPIC changes column topic is displayed in', () => {
     // Given initial state
-    const initialState : KanbanState = {
+    let initialState : KanbanState = {
         columns: [
           {
             id: "1",
@@ -24,9 +26,9 @@ test('MOVE_TOPIC changes column topic is displayed in', () => {
         ],
         topicBeingCreated: false
     };
-
+    
     // When action is reduced
-    const resultingState = reduce(initialState, moveTopic(
+    let action = moveTopic(
         {
             columnId: "1",
             verticalIndex: 0
@@ -35,9 +37,65 @@ test('MOVE_TOPIC changes column topic is displayed in', () => {
             columnId: "2",
             verticalIndex: 0
         }
-    ));
+    );
+    let resultingState = reduce(initialState, action);
 
     // Then Topic "a" should be in column "2" index 0
     var column2 = resultingState.columns.find((column) => column.id === "2");
-    expect(column2.topics[0].id === "a");
+    expect(column2.topics[0].id).toEqual("a");
+});
+
+test('MOVE_TOPIC positions topic in new column correctly', () => {
+    // Given initial state
+    let initialState : KanbanState = {
+        columns: [
+          {
+            id: "1",
+            name: "To Discuss",
+            topics: [
+              {
+                id: 'a',
+                name: 'test',
+                description: 'test description'
+              }
+            ]
+          },
+          {
+            id: "2",
+            name: "Discussing",
+            topics: [
+                {
+                  id: 'b',
+                  name: 'test',
+                  description: 'test description'
+                },
+                {
+                  id: 'c',
+                  name: 'test',
+                  description: 'test description'
+                }
+              
+              ]
+          }
+        ],
+        topicBeingCreated: false
+    };
+
+    // When action is reduced
+    let action = moveTopic(
+        {
+            columnId: "1",
+            verticalIndex: 0
+        },
+        {
+            columnId: "2",
+            verticalIndex: 1
+        }
+    );
+
+    let resultingState = reduce(initialState, action);
+
+    // Then Topic "a" should be in column "2" index 0
+    var column2 = resultingState.columns.find((column) => column.id === "2");
+    expect(column2.topics[1].id).toEqual("a");
 });
