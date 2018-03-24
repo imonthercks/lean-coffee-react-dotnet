aec7d11b64aac35207bb3e79959136aeb001e112
CJ Russell <imonthercks@gmail.com>
2018-03-24 15:47:44 -0500

    refactord App.test.js to include react-router dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 
-import reducer from './reducers'
+import createHistory from 'history/createBrowserHistory'
+
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+
+import kanbanReducer from './reducers/kanban'
+
+//import reducer from './reducers'
 
 test('renders without crashing', () => {
-  const store = createStore(reducer);
-  const div = document.createElement('div');
+// Create a history of your choosing (we're using a browser history in this case)
+  const history = createHistory()
+
+  // Build the middleware for intercepting and dispatching navigation actions
+  const middleware = routerMiddleware(history)
+
+  const target = document.getElementById('root');
+  const store = createStore(
+    combineReducers({
+      kanban: kanbanReducer,
+      router: routerReducer
+    }),
+    applyMiddleware(middleware)
+  )
+
+const div = document.createElement('div');
   ReactDOM.render(
-  <Provider store={store}>
-    <div>
-      <App />
-    </div>
-  </Provider>
-  , div);
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <App />
+        </div>
+      </ConnectedRouter>
+    </Provider>
+    , div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
